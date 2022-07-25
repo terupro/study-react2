ab9a40ac9c64855ec387c1803bfc28382d42ee54
‘terupro’ <terupro911@gmail.com>
2022-07-26 08:53:56 +0900

    ✨ APIを叩いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,33 +1,34 @@
 import classes from "src/styles/Home.module.css";
 import Head from "next/head";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/Footer";
+import { useCallback, useEffect, useState } from "react";
 
-const Home = (props) => {
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-
-      {props.isShow ? <h1>{props.count}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="index" />
-      <Footer />
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : null}
     </div>
   );
 };
