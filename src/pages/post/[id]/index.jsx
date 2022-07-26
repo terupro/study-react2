80d7e228fcd0b0d9a21e49e50a3dd4ab26959e0a
‘terupro’ <terupro911@gmail.com>
2022-07-26 19:34:36 +0900

    ✨ ユーザーのfetch、fetcherの共通化

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -1,17 +1,12 @@
 import classes from "src/styles/Home.module.css";
-import Head from "next/head";
 import { Header } from "src/components/Header";
-import { useRouter } from "next/router";
+import { Post } from "src/components/Post";
 
 const PostId = () => {
-  const router = useRouter();
   return (
     <div className={classes.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   );
 };
